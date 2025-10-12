92a8fccb310dc44098a261cdc226d39a68882f91
Wisenk <wisenkmr93@gmail.com>
2025-10-12 16:39:44 +0900

    javascript udemy

--- myapp.js
+++ myapp.js
@@ -8,6 +8,8 @@ console.log(num);
 console.log("string", 10.11, true, false, null, num);
 
 
+/*learning functions or subroutines*/
+
 function makecoffee(sugar, milk) 
 {
     var instructions = "Boil water";
@@ -18,3 +20,11 @@ function makecoffee(sugar, milk)
     return instructions;
 }
 console.log(makecoffee(2, 50));
+
+
+/*learning objects and arrays*/
+var car = {
+  color : "red", //property
+  speed : 200,
+  drive : function() { return "drive";} //method
+};
