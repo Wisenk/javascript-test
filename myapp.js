9359dc5e33f933f547fd865417c2fa1316dc9f97
Wisenk <wisenkmr93@gmail.com>
2025-10-13 00:19:37 +0900

    javascript udemy ch 18

--- myapp.js
+++ myapp.js
@@ -23,8 +23,41 @@ console.log(makecoffee(2, 50));
 
 
 /*learning objects and arrays*/
-var car = {
+
+var car = { //object
   color : "red", //property
   speed : 200,
   drive : function() { return "drive";} //method
 };
+
+var shoppinglist = [ //array
+  "Apple",
+  "Orange",
+  "Pear"
+];
+
+
+var car = { 
+  make: "volvo",
+  speed: 160,
+  engine: {
+    size: 2.0,
+    make: "bmw",
+    fuel: "petrol",
+    pistons: [ {maker: "BMW"}, {maker: "BMW"} ]
+  },
+  drive: function() { return "drive"; }
+};
+
+var array = [
+  "string",
+  100,
+  [ "embed", 200 ],
+  { car:"ford" },
+  function() { return "drive"; }
+];
+
+console.log(car.make);
+
+
+array.splice //splice command for adding and deleting udemy javascript essentials chapter 18
